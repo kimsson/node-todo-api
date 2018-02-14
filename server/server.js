cd09c932f5ebb28923983303d4202fc8dcd1c712
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-02-14 22:21:32 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e)
   })
 })
+app.get('/todos', (req, res) => {
+  Todo.find({}).then((todos) => {
+    res.send({
+      todos,
+      code: 'Status code'
+    });
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 app.listen(3000, () => {
   console.log(`Started port on 3000`);
 })
