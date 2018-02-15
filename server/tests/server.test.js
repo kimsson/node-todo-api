00c533ef201180a3ea354ea1507bb7dd9a70e4b0
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-02-15 14:16:27 +0100

    Add test cases for GET /todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const { ObjectID } = require('mongodb');
+
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 },{
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -66,6 +70,31 @@ describe('GET /todos', () => {
         expect(res.body.todos.length).toBe(2);
       })
       .end(done);
-
   });
 });
+// 5a8564cee4c2255640f21c07
+describe('GET /todos/:id', () => {
+  it('should return a todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done);
+  })
+
+  it('sohuld return a 404 if todo not found', (done) => {
+    var hexID = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexID}`)
+      .expect(404)
+      .end(done)
+  })
+  it('should return 404 non-object ids', (done) => {
+    request(app)
+      .get('/todos/124abc')
+      .expect(404)
+      .end(done)
+  })
+})
