6131ead9bb89e816c339befd216ca1ba49bdf8e6
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-02-15 11:51:29 +0100

    GET /todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
     });
   }, (e) => {
     res.status(400).send(e);
+  })
+})
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(req.params.id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.status(200).send({
+      code: 'Todo found',
+      todo
+    })
+  }).catch((e) => {
+    res.status(400).send();
   })
 })
 app.listen(3000, () => {
