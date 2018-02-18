ccf5fbe3dd7689d97f8ab2a39cd8010182e5329f
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-02-18 13:20:49 +0100

    Add GET /users/me authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -59,6 +59,22 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    console.log('Unable to verify token ', e);
+  }
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+}
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 
 const port = process.env.PORT || 3000;
 
@@ -121,6 +122,11 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   })
 })
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+});
 
 app.listen(port, () => {
   console.log(`Started port on ${port}`);
