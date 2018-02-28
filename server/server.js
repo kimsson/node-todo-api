505976e6f88439057d997947cdd1b36bdbf1d46e
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-02-28 21:08:40 +0100

    UserController route added

--- server/server.js
+++ server/server.js
@@ -20,10 +20,15 @@ app.use(bodyParser.json());
 var router = express.Router();
 
 var todoController = require('./routes/todoController');
+var userController = require('./routes/userController');
 
 app.use('/', router);
 app.use('/todos', authenticate);
-app.use('/todos', todoController)
+app.use('/todos', todoController);
+
+app.use('/users/me', authenticate);
+app.use('/users', userController);
+
 // app.post('/todos', authenticate, (req, res) => {
 //   var todo = new Todo({
 //     text: req.body.text,
@@ -123,51 +128,51 @@ app.use('/todos', todoController)
 //   })
 // })
 
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
-
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then((token) => {
-    res.header('x-auth', token).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  })
-})
-
-app.get('/users', (req, res) => {
-  User.find({}).then((users) => {
-    res.send({
-      users,
-    });
-  }, (e) => {
-    res.status(400).send(e);
-  })
-});
-
-app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user)
-});
-
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  User.findByCredentials(body.email, body.password).then((user) => {
-    user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
-    })
-  }).catch((e) => {
-    res.status(400).send(e);
-  })
-})
-
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
-    res.status(200).send();
-  }, () => {
-    res.status(400).send();
-  });
-});
+// app.post('/users', (req, res) => {
+//   var body = _.pick(req.body, ['email', 'password']);
+//   var user = new User(body);
+//
+//   user.save().then(() => {
+//     return user.generateAuthToken();
+//   }).then((token) => {
+//     res.header('x-auth', token).send(user);
+//   }).catch((e) => {
+//     res.status(400).send(e);
+//   })
+// })
+//
+// app.get('/users', (req, res) => {
+//   User.find({}).then((users) => {
+//     res.send({
+//       users,
+//     });
+//   }, (e) => {
+//     res.status(400).send(e);
+//   })
+// });
+//
+// app.get('/users/me', authenticate, (req, res) => {
+//   res.send(req.user)
+// });
+//
+// app.post('/users/login', (req, res) => {
+//   var body = _.pick(req.body, ['email', 'password']);
+//   User.findByCredentials(body.email, body.password).then((user) => {
+//     user.generateAuthToken().then((token) => {
+//       res.header('x-auth', token).send(user);
+//     })
+//   }).catch((e) => {
+//     res.status(400).send(e);
+//   })
+// })
+//
+// app.delete('/users/me/token', authenticate, (req, res) => {
+//   req.user.removeToken(req.token).then(() => {
+//     res.status(200).send();
+//   }, () => {
+//     res.status(400).send();
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Started port on ${port}`);
