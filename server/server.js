8dace63c6e2fd63aaf835c037cb4112c8abaf1e5
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-03-04 16:38:26 +0100

    Access control allow origin localhost

--- server/server.js
+++ server/server.js
@@ -21,6 +21,26 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    // res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(bodyParser.json());
 //Routes
 var router = express.Router();
