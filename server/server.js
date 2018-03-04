47c8f8194f03ad3201b8d9ea9d72f46c76e15418
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-03-04 20:55:36 +0100

    Req headers origin

--- server/server.js
+++ server/server.js
@@ -28,8 +28,8 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    // res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Origin", 'https://plant-surveillance-app.herokuapp.com/');
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    // res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
